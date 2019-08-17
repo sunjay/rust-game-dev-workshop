845b10125c34a92676c898e023fb7a248f84302c
Sunjay Varma <varma.sunjay@gmail.com>
2019-08-17 15:54:50 -0600

    Creating a window with a gray background (basic game loop)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,62 @@
-fn main() {
-    println!("Hello, world!");
+use std::thread;
+use std::error::Error;
+use std::time::Duration;
+
+use sdl2::{
+    event::Event,
+    keyboard::Keycode,
+    pixels::Color,
+};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // Initialize the SDL2 library
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
+
+    // Create a window with the given title and dimensions
+    let window = video_subsystem.window("Minimal Game", 800, 600)
+        .position_centered()
+        .build()?;
+
+    // Create a canvas that draws on the window
+    let mut canvas = window.into_canvas().build()?;
+
+    let mut event_pump = sdl_context.event_pump()?;
+    // A labelled loop can be used with `break` even from inside another loop
+    'running: loop {
+        // Handle all of the events available right now
+        for event in event_pump.poll_iter() {
+            match event {
+                // Quit the game if the window is closed or if the escape key is pressed
+                Event::Quit {..} |
+                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'running
+                },
+                _ => {}
+            }
+        }
+
+        // Update game state
+        //TODO
+
+        // Draw the game onto the screen
+        canvas.set_draw_color(Color::RGB(128, 128, 128));
+        canvas.clear();
+        //TODO: Render here
+        canvas.present();
+
+        // Manage the timing of the game so that the loop doesn't go too quickly or too slowly.
+        //
+        // Time stepping is a complex topic. We're simplifying things by just always assuming that
+        // 1/60 seconds has passed in each iteration of the loop. 1/60th of a second is 60 FPS.
+        // There are *many* downsides to the code as it is below, but it's good enough as a
+        // starting point.
+        //
+        // For more information and some more robust approaches:
+        // * http://web.archive.org/web/20190506122532/http://gafferongames.com/post/fix_your_timestep/
+        // * https://www.gamasutra.com/blogs/BramStolk/20160408/269988/Fixing_your_time_step_the_easy_way_with_the_golden_48537_ms.php
+        thread::sleep(Duration::from_nanos(1_000_000_000 / 60))
+    }
+
+    Ok(())
 }
