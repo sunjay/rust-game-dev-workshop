b67def37271ce86e92626c6c61128579d020fee9
Sunjay Varma <varma.sunjay@gmail.com>
2019-08-20 19:01:37 -0600

    Exercise #1 template

--- 0-without-ecs/src/goal.rs
+++ 0-without-ecs/src/goal.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code, unused_variables)] //TODO(EX#1): remove this line
+
 use sdl2::{
     rect::{Point, Rect},
     render::{Texture, WindowCanvas},
@@ -13,34 +15,21 @@ pub struct Goal {
 impl Goal {
     /// Creates a new goal
     pub fn new(position: Point, texture: usize) -> Self {
-        Self {
-            position,
-            texture,
-        }
+        unimplemented!() //TODO(EX#1)
     }
 
     /// Returns a rectangle that tightly encompasses the goal in the world coordinate system
     pub fn bounding_box(&self) -> Rect {
-        // This is different from the size of the sprite because we only want the visible region,
-        // not any surrounding transparent pixels
-        Rect::from_center(self.position, 92, 116)
+        //TODO(EX#1): Return a rectangle that tightly surrounds `self.position` and only contains
+        // the visible portion of the sprite. You will need to measure the sprite dimensions.
+        unimplemented!()
     }
 
     /// Draw the goal onto the given canvas
     pub fn render(&self, canvas: &mut WindowCanvas, textures: &[Texture]) -> Result<(), String> {
-        let (sprite_x, sprite_y) = (0, 0);
-        let (sprite_width, sprite_height) = (128, 128);
-        let sprite_rect = Rect::new(sprite_x, sprite_y, sprite_width, sprite_height);
-
-        // The screen coordinate system has (0, 0) in its top-left corner whereas the
-        // world coordinate system has (0, 0) in the center of the screen.
-        let (width, height) = canvas.output_size()?;
-        let screen_pos = self.position + Point::new((width/2) as i32, (height/2) as i32);
-        let screen_rect = Rect::from_center(screen_pos, sprite_width, sprite_height);
-
-        // Copy the sprite onto the canvas
-        canvas.copy(&textures[self.texture], sprite_rect, screen_rect)?;
-
-        Ok(())
+        //TODO(EX#1): Copy and paste the render method body from either player.rs or enemy.rs and
+        // make the necessary adjustments to render the goal texture.
+        // HINT: If there is no animation, do you still need all the same calculations?
+        unimplemented!()
     }
 }
